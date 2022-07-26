614e7d667c52c511b8206172ee491b6f44c5a678
Victor Han <liyanghan1112@gmail.com>
2022-07-26 10:50:40 -0500

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Victor Han on 2022/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
