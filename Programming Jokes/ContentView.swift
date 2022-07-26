14b4f4b971d3b3b9d01fcf870c850896d6101504
Victor Han <liyanghan1112@gmail.com>
2022-07-26 11:20:00 -0500

    Got API key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -25,12 +25,7 @@ struct ContentView: View {
     }
     
     func getJokes() {
-            jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                              punchline: "Because DEC 25 = OCT 31"))
-            jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                              punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-            jokes.append(Joke(setup: "There are 10 types of people in the world",
-                              punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=d582ffb81bmsh2b53131e0f4dbedp18510bjsnea5f7f34cb91"
         }
 }
 
